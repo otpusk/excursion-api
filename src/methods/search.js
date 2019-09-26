537cf464d7c5c91bb3c632af3c1694a300072c05
fishchenko <aleksandr.fishchenko@gmail.com>
2019-09-26 11:59:27 +0300

    change excursion search response parse

--- src/methods/search.js
+++ src/methods/search.js
@@ -2,8 +2,7 @@ import { endpoints } from '../config';
 import { call } from '../fn';
 
 export async function getSearch (query) {
-    const [rawtours] = await call(endpoints.search, { query });
-    const tours = rawtours.map((tour) => Object.values(tour)[0]);
+    const tours = await call(endpoints.search, { query });
 
     return tours;
 }
