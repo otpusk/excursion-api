a878f8015c18c87c50519e09f84553def5835cdf
fluoxe7ine <locbkolocb@gmail.com>
2019-12-30 03:40:58 +0200

    Added advertId, officeId, agencyId and zoom field to getAgencies normalizer

--- src/playground.js
+++ src/playground.js
@@ -6,15 +6,15 @@ async function main () {
     const data = await getAgencies({
         token:  '1b204-25f04-4b78c-0b089-e27ea',
         params: {
-            cruiseId:   40671,
-            date:       '2019-12-28',
-            operatorId: 465,
-            variantId:  2022263105,
-            cityFrom:   '1',
-            deptCity:   '1397',
-            hash:       '',
+            cruiseId:   69891,
+            operatorId: 498,
+            date:       '2020-01-04',
+            variantId:  '2045690983',
+            cityFrom:   1,
         },
     });
+
+    console.log(data);
 }
 
 main();
