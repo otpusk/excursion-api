f8d8d6c400acfc621aeff8dc954acda5a23b66c9
Oleksander Kozyriev <oleksander.kozyriev@gmail.com>
2020-01-14 13:20:11 +0200

    Added escape html function

--- src/methods/excursion.js
+++ src/methods/excursion.js
@@ -1,8 +1,28 @@
 import { endpoints } from '../config';
 import { call } from '../fn';
+import { escapeHtml } from '../helpers';
 
 export async function getExcursion (excursion) {
-    const data = await call(`${endpoints.getExcursion}/${excursion}`);
+    const tour = await call(`${endpoints.getExcursion}/${excursion}`);
 
-    return data;
+    // escaping unsafe characters in html response
+    const { excursion : { description, days }} = tour;
+
+    Object.keys(description).forEach((key) => {
+        if (description[key]) {
+            description[key] = escapeHtml(description[key]);
+        }
+    });
+
+    days.forEach((day, index) => {
+        const { description : dayDescription } = day;
+
+        if (dayDescription) {
+            days[index].description = escapeHtml(dayDescription);
+        }
+
+        console.log(day);
+    });
+
+    return tour;
 }
