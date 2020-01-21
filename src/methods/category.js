1345316ee3411155d18ced0af3cd7aed2e769b6b
Oleksander Kozyriev <oleksander.kozyriev@gmail.com>
2020-01-21 12:39:59 +0200

    Rewrited getCategory method from GET to POST

--- src/methods/category.js
+++ src/methods/category.js
@@ -2,8 +2,7 @@ import { endpoints } from '../config';
 import { call } from '../fn';
 
 export async function getCategory (alias) {
-    const data = await call(`${endpoints.getCategory}/${alias}`);
+    const data = await call(endpoints.getCategory, { body: true, query: { alias }});
 
     return data;
 }
-
--- src/playground.js
+++ src/playground.js
@@ -1,12 +1,9 @@
 import {
-    getAgencies
+    getCategory
 } from './index.js';
 
 async function main () {
-    const data = await getAgencies({
-        params: { cruiseId: 65091, date: '2020-01-26', variantId: 2041690, cityFrom: 1, deptCity: 1397, operator: 464 },
-        token:  '1b204-25f04-4b78c-0b089-e27ea',
-    });
+    const data = await getCategory('/excursion/c-catholic-christmas/');
 
     console.log(data);
 }
